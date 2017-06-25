bba9761af1ce968d1823c1877b0f9f04bdbcf010
Massimiliano Calandrelli <massimiliano.calandrelli@gmail.com>
2017-06-26 01:04:59 +0200

    added windows event logger

--- log/classic_log.go
+++ log/classic_log.go
@@ -1,21 +1,11 @@
 package gu_log
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
-
 const (
 	LOG_ERROR = iota
 	LOG_WARNING
 	LOG_INFO
 	LOG_DEBUG
 )
-
-type LogLevel uint
 
 var (
 	NIL_LOGGER Logger = &nil_logger{}
@@ -24,101 +14,13 @@ var (
 	DebugLog Logger = NIL_LOGGER
 )
 
-func (l *LogLevel) Set(value string) error {
-	s := strings.ToLower(value)
-	switch s {
-	case "error":
-		*l = LOG_ERROR
-	case "warning":
-		*l = LOG_WARNING
-	case "info":
-		*l = LOG_INFO
-	case "debug":
-		*l = LOG_DEBUG
-	default:
-		if strings.HasPrefix(s, "debug") {
-			s = strings.Replace(s, "debug", "", 1)
-		}
-		detail, err := strconv.ParseInt(s, 10, 32)
-		if err != nil {
-			return err
-		}
-		*l = LogLevel(detail + LOG_DEBUG)
-	}
-	return nil
-}
-
-func (l LogLevel) String() string {
-	switch l {
-	case LOG_ERROR:
-		return "ERROR"
-	case LOG_WARNING:
-		return "WARNING"
-	case LOG_INFO:
-		return "INFO"
-	case LOG_DEBUG:
-		return "DEBUG"
-	}
-	return fmt.Sprintf("DBG%d", l-LOG_DEBUG)
-}
-
 type Logger interface {
-	Info(msg string)
-	Warning(msg string)
-	Error(msg string)
-	Debug(msg string)
-	Custom(level LogLevel, msg string)
+	Info(msg string, args ...interface{})
+	Warning(msg string, args ...interface{})
+	Error(msg string, args ...interface{})
+	Debug(msg string, args ...interface{})
+	Custom(level LogLevel, msg string, args ...interface{})
 	SetLevel(level LogLevel)
 	GetCurrentLevel() LogLevel
+	MessageId(id uint32) Logger
 }
-
-type std_logger_wrapper struct {
-	logger *log.Logger
-	level  LogLevel
-}
-
-func (l *std_logger_wrapper) SetLevel(level LogLevel) {
-	if level >= LOG_ERROR {
-		l.level = level
-	}
-}
-
-func (l *std_logger_wrapper) GetCurrentLevel() LogLevel {
-	return l.level
-}
-
-func (l *std_logger_wrapper) Info(msg string) {
-	l.Custom(LOG_INFO, msg)
-}
-
-func (l *std_logger_wrapper) Warning(msg string) {
-	l.Custom(LOG_WARNING, msg)
-}
-
-func (l *std_logger_wrapper) Error(msg string) {
-	l.Custom(LOG_ERROR, msg)
-}
-
-func (l *std_logger_wrapper) Debug(msg string) {
-	l.Custom(LOG_DEBUG, msg)
-}
-
-func (l *std_logger_wrapper) Custom(level LogLevel, msg string) {
-	if level <= l.level {
-		l.logger.Println(level.String(), " ", msg)
-	}
-}
-
-func StdLogger() Logger {
-	return &std_logger_wrapper{logger: log.New(os.Stderr, "", log.LstdFlags), level: LOG_INFO}
-}
-
-func CustomLogger(logger *log.Logger) Logger {
-	return &std_logger_wrapper{logger: logger, level: LOG_INFO}
-}
-
-type nil_logger struct {
-	std_logger_wrapper
-}
-
-func (l *nil_logger) Custom(level LogLevel, msg string) {}
---
+++ log/logger_wrapper_windows.go
@@ -0,0 +1,72 @@
+//
+// +build windows
+//
+package gu_log
+
+import (
+	"fmt"
+
+	"golang.org/x/sys/windows/svc/debug"
+)
+
+type win_logger_wrapper struct {
+	logger debug.Log
+	level  LogLevel
+	msgid  uint32
+}
+
+func (l *win_logger_wrapper) SetLevel(level LogLevel) {
+	if level >= LOG_ERROR {
+		l.level = level
+	}
+}
+
+func (l win_logger_wrapper) GetCurrentLevel() LogLevel {
+	return l.level
+}
+
+func (l win_logger_wrapper) MessageId(msgid uint32) Logger {
+	return &win_logger_wrapper{logger: l.logger, level: l.level, msgid: msgid}
+}
+
+func (l win_logger_wrapper) Custom(level LogLevel, msg string, args ...interface{}) {
+	if l.level >= level {
+		m := fmt.Sprintf(msg, args...)
+		switch level {
+		case LOG_ERROR:
+			l.logger.Error(l.msgid, m)
+		case LOG_WARNING:
+			l.logger.Warning(l.msgid, m)
+		case LOG_INFO:
+			l.logger.Info(l.msgid, m)
+		default:
+			l.logger.Info(l.msgid, fmt.Sprintf("[%s] %s", level.LogString(), m))
+		}
+	}
+}
+
+func (l win_logger_wrapper) Debug(msg string, args ...interface{}) {
+	l.Custom(LOG_DEBUG, msg)
+}
+
+func (l win_logger_wrapper) Info(msg string, args ...interface{}) {
+	if l.level >= LOG_INFO {
+		l.logger.Info(l.msgid, fmt.Sprintf(msg, args...))
+	}
+}
+
+func (l win_logger_wrapper) Warning(msg string, args ...interface{}) {
+	if l.level >= LOG_WARNING {
+		l.logger.Warning(l.msgid, fmt.Sprintf(msg, args...))
+	}
+}
+
+func (l win_logger_wrapper) Error(msg string, args ...interface{}) {
+	if l.level >= LOG_ERROR {
+		l.logger.Error(l.msgid, fmt.Sprintf(msg, args...))
+	}
+}
+
+func GetWindowsEventLogger(logger debug.Log) Logger {
+	return &win_logger_wrapper{logger: logger, level: LOG_INFO, msgid: 0}
+}
